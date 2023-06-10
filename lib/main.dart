2761284b5486adecfce62218a524279183d75a39
Miguel <miguel.gf.130@gmail.com>
2023-06-10 09:12:00 -0600

    [Main] Se agrega lógica de splash screen y rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-import 'screens/home_screen.dart';
+import '/screens/splash_screen.dart';
+import 'routes/get_pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,16 +10,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
+      getPages: GetPages().getPages,
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+import '../utils/assets_utils.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _esInicio = true;
+  @override
+  Future<void> didChangeDependencies() async {
+    if (_esInicio) {
+      await Future<void>.delayed(const Duration(seconds: 4));
+      setState(() {
+        _esInicio = false;
+      });
+      Get.offNamed('home-screen');
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SizedBox(
+            width: Get.width * 0.6,
+            child: Lottie.asset(
+              AssetsUtils.splashAnimation,
+              fit: BoxFit.cover,
+              filterQuality: FilterQuality.high,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
