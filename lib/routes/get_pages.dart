e17f038118200774a3ad4103989f81cf49f415c3
Miguel <miguel.gf.130@gmail.com>
2023-06-10 20:00:27 -0600

    [Routes] Se separa nombre de rutas en neuvo archivo para poderlas utilizar como constantes y disminuir el error al usarlas y no teclearlas a mano

--- lib/routes/get_pages.dart
+++ lib/routes/get_pages.dart
@@ -1,11 +1,18 @@
 import 'package:get/get.dart';
 
 import '../screens/home_screen.dart';
+import '../screens/nominas_screen.dart';
+import '../screens/operadores_screen.dart';
 import '../screens/splash_screen.dart';
+import 'name_pages.dart';
 
 class GetPages {
   List<GetPage<dynamic>> getPages = <GetPage<dynamic>>[
-    GetPage<dynamic>(name: '/splash-screen', page: () => const SplashScreen()),
-    GetPage<dynamic>(name: '/home-screen', page: () => const HomeScreen()),
+    GetPage<dynamic>(name: nameSplashScreen, page: () => const SplashScreen()),
+    GetPage<dynamic>(name: nameHomeScreen, page: () => const HomeScreen()),
+    GetPage<dynamic>(
+        name: nameNominasScreen, page: () => const NominasScreen()),
+    GetPage<dynamic>(
+        name: nameOperadoresScreen, page: () => const OperadoresScreen()),
   ];
 }
---
+++ lib/routes/name_pages.dart
@@ -0,0 +1,4 @@
+const String nameSplashScreen = '/splash-screen';
+const String nameHomeScreen = '/home-screen';
+const String nameNominasScreen = '/operadores-screen';
+const String nameOperadoresScreen = '/nominas-screen';
