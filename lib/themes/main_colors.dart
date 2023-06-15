a7757b5fc5eca62b59f4fc12f50861120046dbbe
Miguel <miguel.gf.130@gmail.com>
2023-06-14 22:55:27 -0600

    [Colors] Se agrega paleta de colores

---
+++ lib/themes/main_colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+Color blanco = const Color.fromRGBO(255, 255, 255, 1.0);
+Color negro = const Color.fromRGBO(0, 0, 0, 1.0);
+Color caption = const Color.fromRGBO(213, 232, 230, 1.0);
+Color primary = const Color.fromRGBO(25, 118, 210, 1.0);
+Color secondary = const Color.fromRGBO(38, 166, 154, 1.0);
+Color accent = const Color.fromRGBO(156, 39, 176, 1.0);
+Color dark = const Color.fromRGBO(29, 29, 29, 1.0);
+Color darkPage = const Color.fromRGBO(18, 18, 18, 1.0);
+Color positive = const Color.fromRGBO(33, 186, 69, 1.0);
+Color negative = const Color.fromRGBO(193, 0, 21, 1.0);
+Color info = const Color.fromRGBO(49, 204, 236, 1.0);
+Color warning = const Color.fromRGBO(242, 192, 55, 1.0);
+Color bodyText = const Color.fromRGBO(0, 0, 0, 1.0);
+Color background = const Color.fromRGBO(230, 230, 230, 1.0);
+Color focusColor = const Color.fromRGBO(25, 118, 210, 1.0);
+Color enabledColor = const Color.fromRGBO(255, 105, 240, 1.0);
+Color disabledColor = const Color.fromRGBO(200, 200, 200, 1.0);
+Color errorColor = const Color.fromRGBO(193, 0, 21, 1.0);
+Color bgCard = const Color.fromRGBO(219, 219, 219, 1.0);
+
+MaterialColor kPrimaryColor = const MaterialColor(
+  0xFF0E7AC7,
+  <int, Color>{
+    50: Color(0xFF0E7AC7),
+    100: Color(0xFF0E7AC7),
+    200: Color(0xFF0E7AC7),
+    300: Color(0xFF0E7AC7),
+    400: Color(0xFF0A7AC7),
+    500: Color(0xFF1976D2),
+    600: Color(0xFF0E7AC7),
+    700: Color(0xFF0E7AC7),
+    800: Color(0xFF0E7AC7),
+    900: Color(0xFF0E7AC7),
+  },
+);
